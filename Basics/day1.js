9fa25471fde785cebc0b61ef8ff4cc8e7f56a4ff
Avinash Dubey <123487788+avidev-creator@users.noreply.github.com>
2024-06-17 05:31:15 +0000

    variables

---
+++ Basics/day1.js
@@ -0,0 +1,26 @@
+// Variables
+
+// We define variables by 3 ways in JS -- 
+// let , const , var
+
+let name = "Avinash"
+
+// In order to display in terminal we have to use CONSOLE.LOG()
+
+// console.log(name)
+
+const age = 32
+
+console.log(age);
+
+// Let is a mutable variable and const is an Immutable variable
+
+name = "Atul"
+console.log(name);
+
+age = 24
+console.log(age);
+
+
+// We do not use VAR 
+// Example - var fname = "Avinash"
