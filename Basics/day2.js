48e7aeb3138bde853e3344cc700ba38cf7528658
Avinash Dubey <123487788+avidev-creator@users.noreply.github.com>
2024-06-18 05:33:52 +0000

    data types

---
+++ Basics/day2.js
@@ -0,0 +1,71 @@
+// Data Types
+
+/*
+1. Primitive Data Type / Non Reference Data Type--
+a. String
+b. Number = 2 to the power 53
+c. BigInt
+d. Boolean
+e. Symbol
+
+2. Non Primitive Data Type / Reference Data Type
+a. Array
+b. Object
+c. Function
+*/
+
+
+//String Data Type --
+
+// String is defined by "" or ''
+let firstName = "Avinash"
+let lastName = 'Dubey'
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(typeof lastName)
+
+// Number Data Type -- 
+
+let number = -30.5864
+// console.log(number);
+// console.log(typeof number);
+
+
+// Boolean Data Type
+
+let isActive = true
+
+// console.log(isActive);
+// console.log(typeof (isActive));
+
+// Symbol
+
+// Symbol is a unique datatype present in javascript
+
+const sym1 = Symbol();
+// console.log(typeof sym1);
+
+// Array
+// Array is a collection of multiple values which can have multiple data types
+
+let array1 = [1,23,4,5,6,7,"Sumit", "Shivam",-3,3.64, true]
+// console.log(array1);
+// console.log(typeof array1);
+
+// console.log(array1[6]);
+// console.log(array1.indexOf("Sumit"));
+// console.log(array1.length);
+
+// Object
+
+// Object keeps the value in the form of Key-pair and can contain multiple values which can have multiple data types
+let myobj1 = {
+    fname : "Avinash",
+    age : 32,
+    age1: 44,
+    isLoggedIn : true
+}
+
+// console.log(myobj1);
+
